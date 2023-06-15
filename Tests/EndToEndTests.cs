d9986e7cee9ed136f47d3454fb73c26ea113f8f7
Jan <jan.herscheid@gmail.com>
2023-06-15 04:00:22 +0200

    Add End to end tests

--- Tests/EndToEndTests.cs
+++ Tests/EndToEndTests.cs
@@ -1,29 +1,84 @@
+using DAL;
+using DTO;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using System.Net;
 
 namespace Tests
 {
     [TestFixture]
-    public class EndToEndTests
+    internal class EndToEndTests
     {
-        private IWebDriver driver;
+        private readonly HttpClient httpClient;
 
-        [SetUp]
-        public void Setup()
+        //Om deze tests te runnen moet je naar bin\Debug\net6.0 directory en de exe van de applicatie runnen
+        public EndToEndTests()
         {
-            driver = new FirefoxDriver();
-        }
-
-        [TearDown]
-        public void Teardown()
-        {
-            driver.Quit();
+            //Voor het weghalen van certificate errors
+            var handler = new HttpClientHandler()
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+            };
+            httpClient = new HttpClient(handler);
+            httpClient.BaseAddress = new Uri("https://localhost:5001/");
         }
 
         [Test]
-        public void Test1()
+        public async Task GetAllMice_Success()
         {
-            Assert.Pass();
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Act
+            var mice = await client.GetAsync("/mouse");
+
+            // Assert
+            Assert.IsNotNull(mice);
+        }
+
+        [Test]
+        public async Task GetAllMods_Success()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Act
+            var mods = await client.GetAsync("/mod");
+
+            // Assert
+            Assert.IsNotNull(mods);
+        }
+
+        [Test]
+        public async Task GetAllMods_Fail()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Act
+            var mods = await client.GetAsync("/mods");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, mods.StatusCode);
+        }
+
+        [Test]
+        public async Task GetAllMice_Fail()
+        {
+            // Arrange
+            using var application = new WebApplicationFactory<Program>();
+            var client = application.CreateClient();
+
+            // Act
+            var mice = await client.GetAsync("/mice");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, mice.StatusCode);
         }
     }
 }
--- Tests/ModTests.cs
+++ Tests/ModTests.cs
@@ -42,5 +42,64 @@ namespace Tests
             mockRepo.Verify(x => x.GetAll(), Times.Once);
             Assert.That(allMods, Is.EquivalentTo(ListInMock));
         }
+
+        [Test]
+        public void AddMod_Success()
+        {
+            //Arrange
+            Mouse BaseMouse = new Mouse { Id = 1, Name = "name2", Brand = "brand 2", Image = "link to image", Sensor = "sensor 2", Weight = 20 };
+
+            var mockRepo = new Mock<IModHandler>();
+            var mouseRepo = new Mock<IMouseHandler>();
+
+            // Zodat ik getById kan gebruiken die in addmod gebruikt wordt
+            mouseRepo.Setup(x => x.getById(It.IsAny<int>())).Returns(BaseMouse);
+
+            MouseMod capturedMod = null;
+            mockRepo.Setup(x => x.AddMod(It.IsAny<MouseMod>()))
+                    .Callback<MouseMod>(mod => capturedMod = mod);
+
+            var _modLogic = new ModLogic(mockRepo.Object, mouseRepo.Object);
+
+            //Act
+            _modLogic.AddMod(1, 23, "a comment", "user1");
+
+            //Assert
+            Assert.IsNotNull(capturedMod, "capturedMod is null");
+            Assert.AreEqual("user1", capturedMod.auth0Id, "auth0Id is niet hetzelfde");
+            Assert.AreEqual(BaseMouse, capturedMod.Base, "Base is niet hetzelfde");
+            Assert.AreEqual("a comment", capturedMod.Comments, "Comments is niet hetzelfde");
+            Assert.AreEqual(23, capturedMod.Weight, "Weight is niet hetzelfde");
+        }
+
+        [Test]
+        public void GetModsByUser_Success()
+        {
+            // Arrange
+            Mouse BaseMouse = new Mouse { Name = "name2", Brand = "brand 2", Image = "link to image", Sensor = "sensor 2", Weight = 20 };
+            List<MouseMod> ListInMock = new List<MouseMod>
+            {
+                new MouseMod{ auth0Id="id", Base=BaseMouse, Comments="Some Comment", Weight = 5},
+                new MouseMod{ auth0Id="id", Base=BaseMouse, Comments="Another Comment", Weight = 15},
+                new MouseMod{ auth0Id="id2", Base=BaseMouse, Comments="Another Comment", Weight = 25}
+            };
+
+            var mockRepo = new Mock<IModHandler>();
+            var mouseRepo = new Mock<IMouseHandler>();
+
+            mockRepo.Setup(x => x.getModsByUser(It.IsAny<string>()))
+                    .Returns((string uid) => ListInMock.Where(mod => mod.auth0Id == uid).ToList());
+
+            var _modLogic = new ModLogic(mockRepo.Object, mouseRepo.Object);
+
+            // Act
+            var selectedMods = _modLogic.getModsByUser("id");
+
+            // Assert
+            List<MouseMod> checkList = ListInMock.Where(x => x.auth0Id == "id").ToList();
+            Assert.That(selectedMods, Is.EquivalentTo(checkList));
+        }
+
+
     }
 }
